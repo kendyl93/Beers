fd5b0dfd356c13c3d8e9634072a8e3d8bff38ce3
Pawel Stanecki <p.stanecki93@gmail.com>
2020-01-25 17:47:43 +0100

    Update modal on click and url

--- index.js
+++ index.js
@@ -2,30 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+  useLocation,
+  useParams
+} from 'react-router-dom';
 
 import Details from './src/Beers/Components/Details';
 import configureStore from './src/model/store';
 import App from './src/App';
+import Modal from './src/UI/Modal/Modal';
 
 import './index.scss';
 
 const store = configureStore();
 
-const WithProvider = ({ store }) => (
-  <Provider store={store}>
-    <Router>
-      <Switch>
+const ModalSwitch = () => {
+  const location = useLocation();
+  const { background } = (location && location.state) || {};
+
+  return (
+    location && (
+      <div>
+        <Switch location={background || location}>
+          <Route path="/">
+            <App />
+          </Route>
+          <Route exact path="/details/:id">
+            <Details />
+          </Route>
+        </Switch>
         <Route path="/details/:id">
-          <Details />
+          <Modal />
         </Route>
-        <Route exact patch="/">
-          <App />
-        </Route>
-      </Switch>
-    </Router>
-  </Provider>
-);
+      </div>
+    )
+  );
+};
+
+const WithProvider = ({ store }) => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <ModalSwitch />
+      </Router>
+    </Provider>
+  );
+};
 
 WithProvider.propTypes = {
   store: PropTypes.object
--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+  useLocation,
+  useParams
+} from 'react-router-dom';
 
-export default class Modal extends React.Component {
-  render() {
-    return <div>Hello Modal</div>;
-  }
-}
+import Details from '../../Beers/Components/Details';
+
+const Modal = () => {
+  const history = useHistory();
+  const { id } = useParams();
+
+  const back = e => {
+    e.stopPropagation();
+    history.goBack();
+  };
+
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        background: 'rgba(0, 0, 0, 0.15)'
+      }}
+    >
+      <div
+        className="modal"
+        style={{
+          position: 'absolute',
+          background: '#fff',
+          top: 25,
+          left: '10%',
+          right: '10%',
+          padding: 15,
+          border: '2px solid #444'
+        }}
+      >
+        <h1>{id}</h1>
+        <Details id={id} />
+        <Link to="/">Close</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
