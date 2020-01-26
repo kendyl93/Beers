caf41d9df5ff958b4624555f26d28cfa7aecba5a
Pawel Stanecki <p.stanecki93@gmail.com>
2020-01-26 18:27:58 +0100

    Added standard deviation

--- src/Beers/Details/DetailsView.js
+++ src/Beers/Details/DetailsView.js
@@ -10,10 +10,40 @@ import WithSimilar from '../../HOC/WithSimilar';
 
 const byId = sourceId => ({ id }) => id === sourceId;
 
+const add = (sum, value) => sum + value;
+
+const average = data => {
+  const sum = data.reduce(add);
+
+  return sum / data.length;
+};
+
+const squareDifference = data =>
+  data.map(value => (value - average(data)) ** 2);
+
+const standardDeviation = data => {
+  const dataSquareDifference = squareDifference(data);
+
+  return Math.sqrt(average(dataSquareDifference));
+};
+
 const DetailsView = ({ beers }) => {
   const { id: currentBeerId } = useParams();
   const byCurrentBeerId = byId(Number(currentBeerId));
   const beer = beers && beers.find(byCurrentBeerId);
+
+  const { ibu: sourceIBU = 0, abv: sourceABV = 0, ebc: sourceEBC = 0 } =
+    beer || {};
+  const KORR =
+    beers &&
+    beers.map(({ ibu = 0, abv = 0, ebc = 0 }) => {
+      const ibuDifference = sourceIBU - ibu;
+      const abvDifference = sourceABV - abv;
+      const ebcDifference = sourceEBC - ebc;
+
+      return standardDeviation([ibuDifference, abvDifference, ebcDifference]);
+    });
+  console.log({ KORR });
   const similar = beers && [beers[0], beers[1], beers[2]]; // TODO
 
   const DetailsWithSimilarBeers = WithSimilar(similar)(Details);
