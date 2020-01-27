6dc6b91dc75b6e64d53b40ec4e0a47c8d23f0c4a
Pawel Stanecki <p.stanecki93@gmail.com>
2020-01-27 18:26:52 +0100

    Added more styling

--- src/Beers/Components/BeersList.js
+++ src/Beers/Components/BeersList.js
@@ -11,10 +11,13 @@ const BeersList = ({ beers }) => {
     <div className="list-wrapper row-spacing">
       {beers ? (
         // eslint-disable-next-line camelcase
-        beers.map(({ id, name, image_url: imageUrl }) => (
+        beers.map(({ id, name, image_url: imageUrl, tagline }) => (
           <Link to={`/details/${id}`} className="item-wrapper" key={id}>
             <img src={imageUrl} alt={name} width="auto" height="200px" />
-            {name}
+            <div className="title-wrapper">
+              <h4>{name}</h4>
+            </div>
+            <p className="tagline">{tagline}</p>
           </Link>
         ))
       ) : (
