f8a219b86b58f0df66527d630d82f0e526fdad5a
Pawel Stanecki <p.stanecki93@gmail.com>
2020-01-27 21:59:14 +0100

    remove unnecesary layout

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-import Layout from './Layout/Layout';
 import BeerReviewer from './BeerReviewer/BeerReviewer';
 import './App.scss';
 
 const App = () => (
-  <Layout>
+  <div>
     <h1 className="main-header">
       <span className="beer">BEER</span>
       <span>GURU</span>
     </h1>
     <BeerReviewer />
-  </Layout>
+  </div>
 );
 
 export default App;
