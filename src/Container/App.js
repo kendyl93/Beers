151a39219f5705b60fbcf5547c3905c43413010d
Pawel Stanecki <p.stanecki93@gmail.com>
2020-01-27 21:57:11 +0100

    Update App()

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,35 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Layout from './Layout/Layout';
 import BeerReviewer from './BeerReviewer/BeerReviewer';
 import './App.scss';
 
-class App extends Component {
-  state = {
-    showModal: this.modal,
-    modalContent: null,
-    backdrop: false,
-    props: {}
-  };
-
-  modal = (modalContent = null, props = {}) => {
-    this.setState({
-      modalContent,
-      props
-    });
-  };
-
-  render() {
-    return (
-      <Layout>
-        <h1 className="main-header">
-          <span className="beer">BEER</span>
-          <span>GURU</span>
-        </h1>
-        <BeerReviewer />
-      </Layout>
-    );
-  }
-}
+const App = () => (
+  <Layout>
+    <h1 className="main-header">
+      <span className="beer">BEER</span>
+      <span>GURU</span>
+    </h1>
+    <BeerReviewer />
+  </Layout>
+);
 
 export default App;
