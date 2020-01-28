0b906083cf40c87e8fe7933ec9981897787eb278
Pawel Stanecki <p.stanecki93@gmail.com>
2020-01-28 10:57:28 +0100

    Refactor suggested beers

--- src/Container/BeerReviewer/SuggestionList/SuggestionList.js
+++ src/Container/BeerReviewer/SuggestionList/SuggestionList.js
@@ -6,151 +6,167 @@ import { connect } from 'react-redux';
 import * as actionsCreator from '../../../store/actions/index';
 import Thumbnail from '../../../Components/Thumbnail/Thumbnail';
 import LoadingOrError from '../../ErrorBoundary/LoadingOrError';
-import {axiosBeerApi} from '../../../api';
+import { axiosBeerApi } from '../../../api';
 import { statusHandler, itemErrorChecker } from '../../../ErrorHandler';
 
 import './SuggestionList.scss';
 
+
 class SuggestionList extends Component {
-	state = {
-		isItemFetching: false,
-		isItemsLoading: false,
-		isError: false,
-		quantity: 3,
-		fetchedItems: [],
-		items: [],
-		loadingItem: null
-	}
+  state = {
+    isItemFetching: false,
+    pending: false,
+    error: false,
+    quantity: 3,
+    fetchedItems: [],
+    items: [],
+    loadingItem: null
+  };
 
-	//download random items
-	randomItem = (index) => {
-		//we provide index to know a place to put a new item
-		this.setState({
-			isItemFetching: true,
-			isError: false
-		},
-			() => {
+  //download random items
+	randomItem = index => {
+		const { fetchedItems } = this.state;
+    //we provide index to know a place to put a new item
+		this.setState(
+			{
+				isItemFetching: true,
+				error: false
+			},
+			async () => {
 				const query = '/random';
-				axiosBeerApi.get(query)
-					.then(res => {
-						if (statusHandler(res)) throw statusHandler(res);
-						return res.data.shift()
+	  
+				try {
+					const response = await axiosBeerApi.get(query);
+	  
+					const maybeError = statusHandler(response);
+					if (maybeError) {
+						throw statusHandler(response);
+					}
+	  
+					const { data } = response;
+	  
+					const dataExist = itemErrorChecker(data);
+					if (dataExist) {
+						return;
+					}
+
+					const item = data.shift();
+					
+					const { fetchedItems } = this.state;
+					const downloadItems = [...fetchedItems];
+					// here is we test what to do with new item,
+					// to splice it or push into the array
+	  
+					if (index) {
+						downloadItems.splice(index, 0, item);
+					} else {
+						downloadItems.push(item);
+					}
+					
+					this.setState({
+						isItemFetching: false,
+						fetchedItems: downloadItems
 					})
-					.catch(er => er)
-					.then(item => {
-						if (itemErrorChecker(item)) return;
-						const downloadItems = [...this.state.fetchedItems];
-						// here is we test what to do with new item,
-						// to splice it or push into the array
-						(index) ? downloadItems.splice(index, 0, item) : downloadItems.push(item)
-						this.setState({
-							isItemFetching: false,
-							fetchedItems: downloadItems
-						})
-					})
-			}
-		)
-	}
-	// download and store items
-	downloadedItems = (id) => {
-		// in this test we find specified id in the fetched items and remove it
-		// allowing to download new one
-		let alreadyFetched = [],
-			index;
-		if (id) {
-			alreadyFetched = [...this.state.fetchedItems];
-			index = alreadyFetched.findIndex((e) => e.id === id);
-			alreadyFetched.splice(index, 1);
-		}
-		// here is we download new items
-		this.setState({
-			isItemsLoading: true,
-			fetchedItems: alreadyFetched
-			// items: []
-		},
-			() => {
-				// deside how many items to be downloaded
-				let i = this.state.quantity - alreadyFetched.length;
-				while (i--) {
-					this.randomItem(index)
+			  
+				} catch (error) {
+					console.error(error)
 				}
-			})
-	}
+			}
+		);
+  };
+  // download and store items
+  downloadedItems = id => {
+    // in this test we find specified id in the fetched items and remove it
+    // allowing to download new one
+    let alreadyFetched = [],
+      index;
+    if (id) {
+      alreadyFetched = [...this.state.fetchedItems];
+      index = alreadyFetched.findIndex(e => e.id === id);
+      alreadyFetched.splice(index, 1);
+    }
+    // here is we download new items
+    this.setState(
+      {
+        pending: true,
+        fetchedItems: alreadyFetched
+        // items: []
+      },
+      () => {
+        // deside how many items to be downloaded
+        let i = this.state.quantity - alreadyFetched.length;
+        while (i--) {
+          this.randomItem(index);
+        }
+      }
+    );
+  };
 
-	renderedBeers = () => {
-		//verification whether the loading is finished and items need to be rendered
-		const {
-			fetchedItems,
-			isItemsLoading,
-			quantity
-		} = this.state;
-		if (fetchedItems.length === quantity && isItemsLoading) {
-			const fetched = [...fetchedItems];
-			this.setState({
-				isItemsLoading: false,
-				items: fetched
-			})
-		}
-	}
+  renderedBeers = () => {
+    //verification whether the loading is finished and items need to be rendered
+    const { fetchedItems, pending, quantity } = this.state;
+    if (fetchedItems.length === quantity && pending) {
+      const fetched = [...fetchedItems];
+      this.setState({
+        pending: false,
+        items: fetched
+      });
+    }
+  };
 
+  componentDidMount = () => {
+    this.downloadedItems();
+  };
 
-
-	componentDidMount = () => {
-		this.downloadedItems()
-	}
-
-	render() {
-		const {
-			isItemsLoading,
-			items, isError } = this.state;
-		const loading = isItemsLoading && (<LoadingOrError error={isError}/>);
-		this.renderedBeers()
-		return (
-			<div className="SuggestionList">
-				<h4 className="title">With this beer people also like next:</h4>
-				{loading}
-				{!isItemsLoading && (<ul className="list">
-					{items.map(item => (
-						<li
-							className="item"
-							key={item.id}
-						>
-							<Link
-								to={`/beer/:${item.id}`}
-								onClick={() => {
-									this.downloadedItems(item.id)
-									this.props.itemStoreHandler(item)
-								}}
-							>
-								<Thumbnail item={item} />
-							</Link>
-						</li>
-					))}
-				</ul>)}
-			</div>
-		)
-	}
+  render() {
+    const { pending, items, error } = this.state;
+    const loading = pending && <LoadingOrError error={error} />;
+    this.renderedBeers();
+    return (
+      <div className="SuggestionList">
+        <h4 className="title">With this beer people also like next:</h4>
+        {loading}
+        {!pending && (
+          <ul className="list">
+            {items.map(item => (
+              <li className="item" key={item.id}>
+                <Link
+                  to={`/beer/:${item.id}`}
+                  onClick={() => {
+                    this.downloadedItems(item.id);
+                    this.props.itemStoreHandler(item);
+                  }}
+                >
+                  <Thumbnail item={item} />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	return {
-		getBeer: state.beerDetails.beer
-	}
-}
+  return {
+    getBeer: state.beerDetails.beer
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-	return {
-		itemStoreHandler: (beer) => dispatch(actionsCreator.getBeer(beer))
-	}
-}
+  return {
+    itemStoreHandler: beer => dispatch(actionsCreator.getBeer(beer))
+  };
+};
 
 SuggestionList.propTypes = {
-	isItemFetching: PropTypes.bool,
-	isItemsLoading: PropTypes.bool,
-	isError: PropTypes.bool,
-	quantity: PropTypes.number,
-	fetchedItems: PropTypes.array,
-	items: PropTypes.array
-}
+  isItemFetching: PropTypes.bool,
+  pending: PropTypes.bool,
+  error: PropTypes.bool,
+  quantity: PropTypes.number,
+  fetchedItems: PropTypes.array,
+  items: PropTypes.array
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(SuggestionList)
+export default connect(mapStateToProps, mapDispatchToProps)(SuggestionList);
